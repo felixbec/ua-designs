bc5f5e00daedd241fecb6ddbff93f21dc7661465
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-05-28 12:14:31 -0500

    MODIFIED index to include new sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
+import Skills from '../sections/Skills';
 import Projects from '../sections/Projects';
 import Writing from '../sections/Writing';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const IndexPage = () => (
   <Layout>
     <Header />
     <Landing />
     <About />
+    <Skills />
     <Projects />
     <Writing />
     <Footer />
