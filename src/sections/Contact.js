1cd1bb6492ebca3fc84dc2fd6220b5b2ec9c31a1
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-05-30 13:40:26 -0500

    MODIFIED to prevent spam

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -73,42 +73,16 @@ const Contact = () => (
               <img src={mailIcon} alt="IMG" />
             </div>
           </Tilt>
-          <form name="contact" method="POST" data-netlify="true">
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Role:{' '}
-                <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select>
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message" />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
 
           <form
             name="contact"
             method="POST"
             data-netlify="true"
+            data-netlify-honeypot="bot-field"
             class="contact100-form validate-form"
           >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <span class="contact100-form-title">Get in touch</span>
 
             <div
