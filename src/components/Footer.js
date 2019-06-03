5734ffcb08100b0a76b867bf70f164d933264035
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-06-03 12:14:51 -0500

    FIXED footer componet bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -116,19 +116,3 @@ const Footer = () => (
 );
 
 export default Footer;
-
-{
-  /*
-  <div className="row">
-    <div class="col-md-2" id="top">
-      <a href="#id">
-        <FontAwesome name="arrow-up" />
-      </a>
-    </div>
-    <div class="col-md-5">
-      <h3>Living, learning, & leveling up one day at a time.</h3>
-    </div>
-    <div class="col-md-5">links</div>
-  </div>
-  */
-}
