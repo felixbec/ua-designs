75305529db393ecbe58855addd3a3ce38d682ccb
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-06-10 09:16:51 -0500

    REFACTORED Header to show navbar on toggle click

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,22 @@ import { SectionLinks } from 'react-scroll-section';
 import Fade from 'react-reveal/Fade';
 import RouteLink from './RouteLink';
 import Logo from './Logo/Portfolio.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import '../styles/header.scss';
+
+const Styles = {
+  navbar: {
+    height: '100vh',
+    width: '100vw',
+  },
+};
 
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
 
 const HeaderContainer = styled(Headroom)`
   .headroom--pinned {
-    background: ${props => props.theme.colors.primaryDark};
+    background: transparent;
   }
 
   position: absolute;
@@ -35,51 +45,86 @@ const formatLinks = allLinks =>
     { links: [], home: null },
   );
 
-const Header = () => (
-  <HeaderContainer>
-    <Fade top>
-      <Flex
-        flexWrap="wrap"
-        justifyContent="space-between"
-        alignItems="center"
-        p={3}
-      >
-        <SectionLinks>
-          {({ allLinks }) => {
-            const { home, links } = formatLinks(allLinks);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCollapsed: false,
+      backgroundColor: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-            const homeLink = home && (
-              <Image
-                src={Logo}
-                width="50px"
-                alt="Portfolio Logo"
-                onClick={home.onClick}
-                style={{
-                  cursor: 'pointer',
-                }}
-              />
-            );
-            const navLinks = links.map(({ name, value }) => (
-              <RouteLink
-                key={name}
-                onClick={value.onClick}
-                selected={value.selected}
-              >
-                {name}
-              </RouteLink>
-            ));
+  handleClick() {
+    this.setState(state => ({
+      isCollapsed: !state.isCollapsed,
+    }));
+    if (this.state.isCollapsed == false) {
+      this.setState({ backgroundColor: 'rgba(1,134,135,0.8)' });
+    } else if (this.state.isCollapsed == true) {
+      this.setState({ backgroundColor: 'transparent' });
+    } else {
+      this.setState({ backgroundColor: 'transparent' });
+    }
+  }
 
-            return (
-              <Fragment>
-                {homeLink}
-                <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
-              </Fragment>
-            );
-          }}
-        </SectionLinks>
-      </Flex>
-    </Fade>
-  </HeaderContainer>
-);
+  render() {
+    return (
+      <Navbar expand="*" style={{ background: this.state.backgroundColor }}>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="button"
+          onClick={this.handleClick}
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <div style={Styles.navbar}>
+            <SectionLinks>
+              {({ allLinks }) => {
+                const { home, links } = formatLinks(allLinks);
+                const homeLink = home && (
+                  <Navbar.Brand>
+                    <Image
+                      src={Logo}
+                      width="50px"
+                      alt="Portfolio Logo"
+                      onClick={home.onClick}
+                      style={{
+                        cursor: 'pointer',
+                      }}
+                    />
+                  </Navbar.Brand>
+                );
+                const navLinks = links.map(({ name, value }) => (
+                  <Nav>
+                    <Nav.Link>
+                      <RouteLink
+                        key={name}
+                        onClick={value.onClick}
+                        selected={value.selected}
+                      >
+                        {name}
+                      </RouteLink>
+                    </Nav.Link>
+                  </Nav>
+                ));
+
+                return (
+                  <Fragment>
+                    <Navbar.Brand>
+                      {homeLink}
+                      <Flex className="links__container" mr={[0, 3, 5]}>
+                        {navLinks}
+                      </Flex>
+                    </Navbar.Brand>
+                  </Fragment>
+                );
+              }}
+            </SectionLinks>
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
 
 export default Header;
