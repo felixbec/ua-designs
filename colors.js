20769f624d6ee63939f67dcb19f6dc46d4b3f795
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-06-14 11:43:11 -0500

    MODIFIED color pattern

--- colors.js
+++ colors.js
@@ -1,6 +1,10 @@
 module.exports = {
   background: '#FFFFFF',
   backgroundDark: '#E0F7FA',
+  screenGray: ' #C5C1c0',
+  steelBlack: '#0a1612',
+  denimGray: '#1a2930',
+  MarigoldYellow: '#f7ce3e',
 
   primary: '#03DAC8',
   primaryLight: '#82F0D7',
