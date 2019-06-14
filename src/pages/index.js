3aa40771218a67e3b1ac617d5ad65b065c3a531a
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-06-14 11:53:33 -0500

    MODIFIED order of sections shown and deleted Process section

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,23 +10,37 @@ import Testimonials from '../sections/Testimonials';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Contact from '../sections/Contact';
-import Parallax from '../sections/Parallax';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/global.scss';
+import ToggleSections from '../components/Toggle';
 
-const IndexPage = () => (
-  <Layout>
-    <Header />
-    <Landing />
-    <About />
-    <Skills />
-    <Testimonials />
-    <Process />
-    <Projects />
-    <Writing />
-    <Contact />
-    <Parallax />
-    <Footer />
-  </Layout>
-);
+class IndexPage extends React.Component {
+  componentDidMount() {
+    const vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <Header />
+        <Landing />
+        <About />
+        <ToggleSections />
+        <Testimonials />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
